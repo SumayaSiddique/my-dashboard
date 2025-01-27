9fc7b08a7096ae4d6bdb08471ae23ffddb01f4c8
Sumaya Siddique <sss.savant99@gmail.com>
2025-01-27 08:20:52 +0600

    Minor Changes

--- src/app/components/PaymentAnalysis.js
+++ src/app/components/PaymentAnalysis.js
@@ -42,7 +42,7 @@ export default function PaymentAnalysis() {
             <div className="mt-4 h-64">
                 <ResponsiveContainer width="100%" height="100%">
                     <LineChart data={data}>
-                        <CartesianGrid strokeDasharray="3 3" />
+                        <CartesianGrid horizontal={true} vertical={false} strokeDasharray="3 3" />
                         <XAxis dataKey="name" />
                         <YAxis />
                         <Tooltip />
